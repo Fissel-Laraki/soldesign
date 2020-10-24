bcfecd21d49d3f2b2320d62fe6d2531e505291d1
Fissel Laraki Mohamed <m.fissel.laraki@gmail.com>
2020-10-24 14:00:20 +0200

    modifié :         script.sql

--- script.sql
+++ script.sql
@@ -1,8 +1,8 @@
--- MariaDB dump 10.17  Distrib 10.5.5-MariaDB, for Linux (x86_64)
+-- MariaDB dump 10.17  Distrib 10.5.6-MariaDB, for Linux (x86_64)
 --
 -- Host: localhost    Database: soldesign
 -- ------------------------------------------------------
--- Server version	10.5.5-MariaDB
+-- Server version	10.5.6-MariaDB
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -409,4 +409,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-09-28 22:51:07
+-- Dump completed on 2020-10-24 13:59:56
